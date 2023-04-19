eff088301884d30bfe2410c7470e18a01e784c52
Timo Marttinen <timo@marttinen.me>
2023-04-19 13:43:57 +0300

    Convert to simple toggle

--- extension.js
+++ extension.js
@@ -11,11 +11,11 @@ const QuickSettings = imports.ui.quickSettings;
 // This is the live instance of the Quick Settings menu
 const QuickSettingsMenu = imports.ui.main.panel.statusArea.quickSettings;
 
-const FeatureMenuToggle = GObject.registerClass(
-class FeatureMenuToggle extends QuickSettings.QuickMenuToggle {
+const FeatureToggle = GObject.registerClass(
+class FeatureToggle extends QuickSettings.QuickToggle {
     _init() {
         super._init({
-            label: 'Charge Control',
+            label: 'Charge Full',
             iconName: 'gnome-power-manager-symbolic',
             toggleMode: true,
         });
@@ -23,72 +23,53 @@ class FeatureMenuToggle extends QuickSettings.QuickMenuToggle {
         // This function is unique to this class. It adds a nice header with an
         // icon, title and optional subtitle. It's recommended you do so for
         // consistency with other menus.
-        this.menu.setHeader('gnome-power-manager-symbolic', 'Feature Header',
-            'Current limit: ' + get() + '%');
         
-        // You may also add sections of items to the menu
-        this._itemsSection = new PopupMenu.PopupMenuSection();
-        this._itemsSection.addAction('60%', () => set(60));
-        this._itemsSection.addAction('80%', () => set(80));
-        this._itemsSection.addAction('100%', () => set(100));
-        this._itemsSection.addAction('Get', () => get());
-        this.menu.addMenuItem(this._itemsSection);
-        
-        function set(percentage) {
-            try {
-                let [, stdout, stderr, status] = GLib.spawn_command_line_sync('sudo ectool fwchargelimit ' + percentage);
-            
-                if (status !== 0) {
-                    if (stderr instanceof Uint8Array)
-                        stderr = ByteArray.toString(stderr);
-            
-                    throw new Error(stderr);
-                }
-            
-                if (stdout instanceof Uint8Array)
-                    stdout = ByteArray.toString(stdout);
-            
-                // Now were done blocking the main loop, phewf!
-                log(stdout);
-                this.menu.setHeader
-            } catch (e) {
-                logError(e);
-            }
-        }
-        function get() {
-            try {
-                let [, stdout, stderr, status] = GLib.spawn_command_line_sync('sudo ectool fwchargelimit');
-            
-                if (status !== 0) {
-                    if (stderr instanceof Uint8Array)
-                        stderr = ByteArray.toString(stderr);
-            
-                    throw new Error(stderr);
-                }
-            
-                if (stdout instanceof Uint8Array)
-                    stdout = ByteArray.toString(stdout);
-            
-                // Now were done blocking the main loop, phewf!
-                log(stdout);
-                this.menu.setHeader
-                return stdout.trim(); 
-            } catch (e) {
-                logError(e);
-            }
-            
-        }
+        this._settings = new Gio.Settings({
+            schema_id: 'org.gnome.shell.extensions.fwchargelimit',
+        });
 
-        // // Add an entry-point for more settings
-        // this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        // const settingsItem = this.menu.addAction('More Settings',
-        //     () => ExtensionUtils.openPrefs());
-            
-        // // Ensure the settings are unavailable when the screen is locked
-        // settingsItem.visible = Main.sessionMode.allowSettings;
-        // this.menu._settingsActions[Extension.uuid] = settingsItem;
+        this._settings.bind('show-indicator',
+            this, 'checked',
+            Gio.SettingsBindFlags.DEFAULT);
+
+        this.connectObject(
+            'clicked', () => this._updateChargeLimit(this._settings.get_boolean('show-indicator')),
+            this);
     }
-});
+    _updateChargeLimit(b) {
+        if (b) {
+            this._setChargeFull()
+        } else {
+            this._setChargeHealthy()
+        }
+    }                
+    _setChargeFull() {
+        this._setChargeLimit(100)
+    }
+    _setChargeHealthy() {
+        this._setChargeLimit(80)
+    }
+    _setChargeLimit(percentage) {
+        try {
+            let [, stdout, stderr, status] = GLib.spawn_command_line_sync('sudo ectool fwchargelimit '+percentage);
+        
+            if (status !== 0) {
+                if (stderr instanceof Uint8Array)
+                    stderr = ByteArray.toString(stderr);
+        
+                throw new Error(stderr);
+            }
+        
+            if (stdout instanceof Uint8Array)
+                stdout = ByteArray.toString(stdout);
+        
+            // Now were done blocking the main loop, phewf!
+            log(stdout);
+        } catch (e) {
+            logError(e);
+        }   
+    }
+})
 
 
 
@@ -103,9 +84,24 @@ class FeatureIndicator extends QuickSettings.SystemIndicator {
         this._indicator = this._addIndicator();
         this._indicator.icon_name = 'gnome-power-manager-symbolic';
 
+        this._settings = new Gio.Settings({
+            schema_id: 'org.gnome.shell.extensions.fwchargelimit',
+        });
+
+        let charge = this._getChargeLimit()
+        if (charge == 100) {
+            this._settings.set_boolean('show-indicator', true)
+        } else {
+            this._settings.set_boolean('show-indicator', false)
+        }
+
+        this._settings.bind('show-indicator',
+            this._indicator, 'visible',
+            Gio.SettingsBindFlags.DEFAULT);
+
         // Create the toggle menu and associate it with the indicator, being
         // sure to destroy it along with the indicator
-        this.quickSettingsItems.push(new FeatureMenuToggle());
+        this.quickSettingsItems.push(new FeatureToggle());
         
         this.connect('destroy', () => {
             this.quickSettingsItems.forEach(item => item.destroy());
@@ -114,6 +110,27 @@ class FeatureIndicator extends QuickSettings.SystemIndicator {
         // Add the indicator to the panel and the toggle to the menu
         QuickSettingsMenu._indicators.add_child(this);
         QuickSettingsMenu._addItems(this.quickSettingsItems);
+    }
+    _getChargeLimit() {
+        try {
+            let [, stdout, stderr, status] = GLib.spawn_command_line_sync('sudo ectool fwchargelimit');
+        
+            if (status !== 0) {
+                if (stderr instanceof Uint8Array)
+                    stderr = ByteArray.toString(stderr);
+        
+                throw new Error(stderr);
+            }
+        
+            if (stdout instanceof Uint8Array)
+                stdout = ByteArray.toString(stdout);
+        
+            // Now were done blocking the main loop, phewf!
+            log(stdout);
+            return stdout.trim(); 
+        } catch (e) {
+            logError(e);
+        }   
     }
 });
 
